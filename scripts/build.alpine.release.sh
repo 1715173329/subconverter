544c4ffec9343a0f7fc96bcfa6464323f0ee9b06
Tindy X <49061470+tindy2013@users.noreply.github.com>
2020-02-02 23:23:40 +0800

    Update build script

--- scripts/build.alpine.release.sh
+++ scripts/build.alpine.release.sh
@@ -3,7 +3,7 @@ mkdir obj
 set -xe
 
 apk add gcc g++ build-base linux-headers cmake make autoconf automake libtool
-apk add openssl-dev openssl-libs-static curl-dev curl-static nghttp2-static zlib-dev rapidjson-dev libevent-dev libevent-static zlib-static pcre2-dev bzip2-static 
+apk add openssl-dev openssl-libs-static curl curl-dev curl-static nghttp2-static zlib-dev rapidjson-dev libevent-dev libevent-static zlib-static pcre2-dev bzip2-static 
 
 git clone https://github.com/jbeder/yaml-cpp
 cd yaml-cpp
